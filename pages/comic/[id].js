017456361235e87a10e02289faadf2269aa8b426
tonny187 <tonyoanthony@gmail.com>
2022-09-05 17:15:04 -0500

    ultimate changes

--- pages/comic/[id].js
+++ pages/comic/[id].js
@@ -1,9 +1,23 @@
 import React from 'react'
+import Head from 'next/head'
+import Image from 'next/image'
+import Header from '../../components/Header'
+
 
 
 const tonnyFunction = () => {
   return (
-    <div>tonnyFunction</div>
+    <>
+      <Head>
+        <title>Xkcd - Comics for developers</title>
+        <meta name="description" content="Comics for developers" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Header />
+      <main>
+        <h1>Comics</h1>
+      </main>
+    </>
   )
 }
 
